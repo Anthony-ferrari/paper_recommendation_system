22b92792c04605e5ab40beecda061b2fe2db2d9f
Anthony-ferrari <anthonyferrari1611@gmail.com>
2021-03-26 20:07:16 -0700

    created venv and req.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, url_for, render_template, request
+# from db_connector.db_connector import connect_to_database, execute_query
+from datetime import datetime 
+app = Flask(__name__)
+# index
+@app.route("/")
+def index():
+    return render_template("index.html")
+# home
+@app.route("/home")
+def home():
+    return render_template("home.html")
